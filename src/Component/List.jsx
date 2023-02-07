b55209e42d7d03e81a411363ab937ceee2121831
Mohammad-Asraful <asrafmail01@gmail.com>
2023-02-07 12:35:33 +0600

    first commit

---
+++ src/Component/List.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function List() {
+   return (
+      <ol>
+         <li>Mohammad</li>
+         <li>Asraful</li>
+         <li>Alam</li>
+         <li>Jehad</li>
+      </ol>
+   )
+}
+
+export default List;
